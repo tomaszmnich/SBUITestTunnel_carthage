44d8977b522fb61aab631ae5a40a5d84f13315b0
Tomasz Mnich <tomasz.mnich@polidea.com>
2018-03-28 09:41:05 +0200

    Initial commit

---
+++ Carthage/Build/iOS/SBUITestTunnel_mainTarget.framework/Headers/SBUITestTunnel_mainTarget.h
@@ -0,0 +1,19 @@
+//
+//  SBUITestTunnel_mainTarget.h
+//  SBUITestTunnel_mainTarget
+//
+//  Created by Tomasz Mnich on 28/03/2018.
+//  Copyright © 2018 Tomasz Mnich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SBUITestTunnel_mainTarget.
+FOUNDATION_EXPORT double SBUITestTunnel_mainTargetVersionNumber;
+
+//! Project version string for SBUITestTunnel_mainTarget.
+FOUNDATION_EXPORT const unsigned char SBUITestTunnel_mainTargetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBUITestTunnel_mainTarget/PublicHeader.h>
+
+
---
+++ SBUITestTunnel_testingTarget/SBUITestTunnel_testingTarget.h
@@ -0,0 +1,19 @@
+//
+//  SBUITestTunnel_testingTarget.h
+//  SBUITestTunnel_testingTarget
+//
+//  Created by Tomasz Mnich on 28/03/2018.
+//  Copyright © 2018 Tomasz Mnich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SBUITestTunnel_testingTarget.
+FOUNDATION_EXPORT double SBUITestTunnel_testingTargetVersionNumber;
+
+//! Project version string for SBUITestTunnel_testingTarget.
+FOUNDATION_EXPORT const unsigned char SBUITestTunnel_testingTargetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBUITestTunnel_testingTarget/PublicHeader.h>
+
+
