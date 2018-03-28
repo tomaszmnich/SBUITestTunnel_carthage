4bcbb25115d5e5751714023f0b5e452ae322d12e
Tomasz Mnich <tomasz.mnich@polidea.com>
2018-03-28 09:23:54 +0200

    Initial Commit

---
+++ SBUITestTunnel/SBUITestTunnel.h
@@ -0,0 +1,19 @@
+//
+//  SBUITestTunnel.h
+//  SBUITestTunnel
+//
+//  Created by Tomasz Mnich on 28/03/2018.
+//  Copyright © 2018 Tomasz Mnich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SBUITestTunnel.
+FOUNDATION_EXPORT double SBUITestTunnelVersionNumber;
+
+//! Project version string for SBUITestTunnel.
+FOUNDATION_EXPORT const unsigned char SBUITestTunnelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBUITestTunnel/PublicHeader.h>
+
+
